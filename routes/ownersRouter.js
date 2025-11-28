1f71531b7c141d497323980542b303af98cc690a
vishalparashar0119 <vishalparashar0119@users.noreply.github.com>
2025-11-28 10:59:43 +0530

    settig config file for multer

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -6,4 +6,19 @@ router.get('/', (req, res) => {
       res.send('Owners Home Page');
 });
 
+router.post('/createProduct', async (req, res) => {
+
+      try {
+            const { productName, price, image, discount, backgroundColor, pannelColor, textColor } = req.body;
+
+            return res.status(200).json({
+                  success: true,
+                  user: req.body
+            })
+      } catch (error) {
+            return res.status(500).json({ success: false, message: error.message })
+      }
+      res.send('create proudct')
+})
+
 export default router;
