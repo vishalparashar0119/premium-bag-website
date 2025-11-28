75c734013f4ff804a98407afcb8136de52e47205
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-11-28 19:04:28 +0530

    intrigated cloudinary in server so the images can directly stored in cloud data base

---
+++ config/cloudnaryMulterConfig.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+
+
+// console.log(process.env.CLOUDINARY_CLOUD_NAME,
+//       process.env.CLOUDINARY_API_KEY,
+//       process.env.CLOUDINARY_API_SECRET)
+
+cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+      cloudinary,
+      params: {
+            folder: 'premiumBagWebsiteProduct',
+            allowed_formats: ['jpg', 'jpeg', 'png', 'webp']
+      }
+});
+
+export const upload = multer({ storage: storage });
+
+export default cloudinary;
