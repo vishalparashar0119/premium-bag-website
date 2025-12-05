24fe296d7e3e6630b1177cf9d64df8326199afa1
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-12-05 12:31:34 +0530

    make some improvement in shop controller and owners controller

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -11,3 +11,24 @@ export const fetchAllProducts = async (req, res) => {
       }
       
 }
+
+export const createProduct = async (req, res) => {
+
+      try {
+            const { productName, price, discount, backgroundColor, pannelColor, textColor } = req.body;
+
+            const imageUrl = req.file.path;
+            const publicId = req.file.filename;
+            
+            const newProduct = await ProductModel.create({productName , price , image : {
+                  imageUrl : imageUrl , publicId : publicId
+            }, discount , backgroundColor , pannelColor , textColor})
+
+            return res.status(200).json({
+                  success: true,
+                  message : 'Product created successfully'
+            })
+      } catch (error) {
+            return res.status(500).json({ success: false, message: error.message })
+      }
+}
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { upload } from '../config/cloudnaryMulterConfig.js';
-import ProductModel from '../models/ProductModel.js';
+import { createProduct } from '../controllers/shopController.js';
 
 const router = express.Router();
 
@@ -8,26 +8,6 @@ router.get('/', (req, res) => {
       res.send('Owners Home Page');
 });
 
-router.post('/createProduct', upload.single('image'), async (req, res) => {
-
-      try {
-            const { productName, price, discount, backgroundColor, pannelColor, textColor } = req.body;
-
-            const imageUrl = req.file.path;
-            const publicId = req.file.filename;
-            
-            const newProduct = await ProductModel.create({productName , price , image : {
-                  imageUrl : imageUrl , publicId : publicId
-            }, discount , backgroundColor , pannelColor , textColor})
-
-            return res.status(200).json({
-                  success: true,
-                  user: newProduct,
-                  fileName : req.file
-            })
-      } catch (error) {
-            return res.status(500).json({ success: false, message: error.message })
-      }
-})
+router.post('/createProduct', upload.single('image'), createProduct )
 
 export default router;
