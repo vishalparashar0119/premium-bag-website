e426139b0afaf2594ca7b00aee27591cbc92c2bb
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-12-06 12:45:37 +0530

    created api to fetch a product using id

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import isLoggedIn from '../middleware/isLoggedIn.js'
+import { fetchSingleProduct } from '../controllers/shopController.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
       res.send('products Home Page');
 });
+
+router.get('/product/:id'  , isLoggedIn , fetchSingleProduct);
 
 export default router;
