3bc0d346895903cc9c51ff1d89b0867036c0eb3d
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-12-26 12:31:06 +0530

    fixed corser policy

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(cors({
-      origin: 'http://localhost:5173',
-      credentials : true
+      origin: "http://localhost:5173",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
