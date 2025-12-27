d13ce87f0fe0425ef7555a0648b5e6c44775bc81
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2025-12-27 18:58:44 +0530

    fix url again

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-      origin: ["http://localhost:5173", `https://premium-bag-website-front-git-afb1cd-vishal-parashar-s-projects.vercel.app`],
+      origin: ["http://localhost:5173", `https://premium-bag-website-frontend.vercel.app`],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"]
